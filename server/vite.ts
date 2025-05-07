32160280c77862406cfb176b66dd22deb3e75436
RiiDEV <fakhri.ilham525@smp.belajar.id>
2025-05-07 15:42:53 +0700

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -1,7 +1,6 @@
 import express, { type Express } from "express";
 import fs from "fs";
-import path from "path";
-import { createServer as createViteServer, createLogger, type ServerOptions } from "vite";
+import path from "path";xcreateViteServer, createL gger, type ServerOptions } from "vite";
 import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
